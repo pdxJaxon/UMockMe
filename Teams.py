3e89b8eba6d6b4e47cf9b7b4ab6bcb705987750a
pdxJaxon <GregoryAJackson@Hotmail.com>
2018-01-24 13:21:56 -0800

    Upgrading to Larger DB and Adding EditProspect

--- Teams.py
+++ Teams.py
@@ -74,33 +74,26 @@ class Team:
         teamFile = open("Teams.json","r")
 
 
-        if(teamFile):
-            ParsedData = json.dumps(teamFile.read());
-            print("File For Teams Read");
-            teamFile.close();
-        else:
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        script = soup.find('script', text=re.compile('nfl\.draft\.tracker\.data'))
+
+        #print(soup)
+
+        #print(script.string)
+
+        print("Team Data Web Scraped");
+
+        # m = re.search("^\s+nfl.global.dt.data\s+=\s+{\"picks\".+",script.string,flags=re.IGNORECASE | re.MULTILINE)
+        #m = re.search("^\s+nfl.draft.tracker.data.teams\s+=\s+{\\r\\n", script.string, flags=re.IGNORECASE | re.MULTILINE)
 
 
-            page = requests.get(url)
+        iFoundStart = script.string.find("nfl.draft.tracker.data.teams")
+        iFoundEnd = script.string.find(";",iFoundStart+1)
 
-            soup = BeautifulSoup(page.content, 'html.parser')
-
-            script = soup.find('script', text=re.compile('nfl\.draft\.tracker\.data'))
-
-            #print(soup)
-
-            #print(script.string)
-
-            print("Team Data Web Scraped");
-
-            # m = re.search("^\s+nfl.global.dt.data\s+=\s+{\"picks\".+",script.string,flags=re.IGNORECASE | re.MULTILINE)
-            #m = re.search("^\s+nfl.draft.tracker.data.teams\s+=\s+{\\r\\n", script.string, flags=re.IGNORECASE | re.MULTILINE)
-
-
-            iFoundStart = script.string.find("nfl.draft.tracker.data.teams")
-            iFoundEnd = script.string.find(";",iFoundStart+1)
-
-            ParsedData = script.string[iFoundStart:iFoundEnd]
+        ParsedData = script.string[iFoundStart:iFoundEnd]
 
 
 
