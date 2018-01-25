c57f30587c05221d50f27fcf27a450e1df218d6c
pdxJaxon <GregoryAJackson@Hotmail.com>
2018-01-24 16:27:35 -0800

    Upgrading to Larger DB and Adding EditProspect

--- Teams.py
+++ Teams.py
@@ -69,48 +69,37 @@ class Team:
         # nfl.draft.tracker.data.teams    = {
 
 
-        year = datetime.date.year
-        fileName = str(year) + "Teams.json"
-        fileExists = os.path.isfile(fileName)
+
         parsedData=""
-        
-        if(fileExists):
-            teamFile = open(str(year) + "Teams.json", "r")
-            parsedData = teamFile.read()
-            teamFile.close()
-            print("Data from Team File")
-        else:
-            url = "http://www.nfl.com/draft/2017/tracker#dt-tabs:dt-by-grade"
-
-            #url = http://feeds.nfl.com/feeds-rs/teams/2017.json
 
 
 
-            page = requests.get(url)
+        url = "http://www.nfl.com/draft/2017/tracker#dt-tabs:dt-by-grade"
 
-            soup = BeautifulSoup(page.content, 'html.parser')
-
-            script = soup.find('script', text=re.compile('nfl\.draft\.tracker\.data'))
-
-            #print(soup)
-
-            #print(script.string)
-
-            print("Team Data Web Scraped");
-
-            # m = re.search("^\s+nfl.global.dt.data\s+=\s+{\"picks\".+",script.string,flags=re.IGNORECASE | re.MULTILINE)
-            #m = re.search("^\s+nfl.draft.tracker.data.teams\s+=\s+{\\r\\n", script.string, flags=re.IGNORECASE | re.MULTILINE)
+        #url = http://feeds.nfl.com/feeds-rs/teams/2017.json
 
 
-            iFoundStart = script.string.find("nfl.draft.tracker.data.teams")
-            iFoundEnd = script.string.find(";",iFoundStart+1)
 
-            ParsedData = script.string[iFoundStart:iFoundEnd]
+        page = requests.get(url)
 
-            teamFile = open(fileName, "w")
-            teamFile.write(ParsedData)
-            teamFile.close()
+        soup = BeautifulSoup(page.content, 'html.parser')
 
+        script = soup.find('script', text=re.compile('nfl\.draft\.tracker\.data'))
+
+        #print(soup)
+
+        #print(script.string)
+
+        print("Team Data Web Scraped");
+
+        # m = re.search("^\s+nfl.global.dt.data\s+=\s+{\"picks\".+",script.string,flags=re.IGNORECASE | re.MULTILINE)
+        #m = re.search("^\s+nfl.draft.tracker.data.teams\s+=\s+{\\r\\n", script.string, flags=re.IGNORECASE | re.MULTILINE)
+
+
+        iFoundStart = script.string.find("nfl.draft.tracker.data.teams")
+        iFoundEnd = script.string.find(";",iFoundStart+1)
+
+        ParsedData = script.string[iFoundStart:iFoundEnd]
 
 
         #print(ParsedData)
